868383d42f84d3ad487adf3ba18e665b6114ec7b
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-01-12 20:54:05 +0900

    Second test done

---
+++ Cypress_Tests/Test2.spec.js
@@ -0,0 +1,49 @@
+
+/// <reference types="cypress" />
+
+describe('Second suite', () => {
+
+    it('Test2', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layout').click()
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]', 'Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+
+    })
+
+    it.only('Test2.1', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layout').click()
+
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then( secondForm => {
+                const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordLabelSecond)
+            })
+
+            cy.wrap(firstForm).find('[for="inputEmail1"]').should('contain', 'Email')
+        })
+
+    })
+})
